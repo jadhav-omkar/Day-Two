363ce3e8727a503b771bdd43455c1775c366be8a
jadhav-omkar <omkaroj26@gmail.com>
2023-03-28 21:01:18 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+var x =10;
+var y=20;
+var z=x+y;
+console.log(z);
+// console.log("it is addition")
+
+var x=30;
+var y=40;
+var z=x-y;
+console.log(z) 
+// console.log("it issubstraction")
+
+var x=50;
+var y=60;
+var z=x*y;
+console.log(z)
+// console.log("it is multiplication")
+
+var x=70;
+var y=80;
+var z=x/y;
+console.log(z)
+// console.log("it is divison")
+
+var x=50;
+var y=50;
+var z=x**y;
+console.log(z)
+// console.log("it is expoentation")
+
+var x=10;
+var y=20;
+var z=x%y;
+console.log(z)
+// console.log("it is reminder")
+
+var x=30;
+var y=40;
+var z=++x;
+console.log(z)
+// console.log("it is increment")
+
+
+var x=50;
+var y=60;
+var z=--y;
+console.log(z)
+// console.log("it is decrement")
